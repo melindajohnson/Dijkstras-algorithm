3eed143e2a55d9fd87504d354f891efdd4a7fffe
meljo <melistajohn@gmail.com>
2020-02-10 17:42:34 -0800

    constructor and destructor

--- Assignment3/Assignment3/Graph.cpp
+++ Assignment3/Assignment3/Graph.cpp
@@ -1,15 +1,50 @@
-//
-//  Graph.cpp
-//  Assignment3
-//
-//  Created by Melinda Johnson on 2/4/20.
-//  Copyright © 2020 Melinda Johnson. All rights reserved.
-//
+
 
 #include <fstream>
 #include "Graph.h"
 using namespace std;
 
+Graph::Graph(){
+   size = 0;
+}
+
+Graph::Graph(Graph& obj1){
+   size = obj1.size;
+   for (int i = 0; i < obj1.MAX_VERTICES; ++i){
+      for (int j = 0; j < obj1.MAX_VERTICES ; ++j){
+         T[i][j] = obj1.T[i][j];
+      }
+   }
+   for(int v = 1; v < obj1.size; v++){
+      vertices[v] = obj1.vertices[v];
+   }
+}
+
+const Graph Graph::operator=(const Graph& right){
+   if (this != &right) {
+      size = right.size;
+      for (int i = 0; i < right.MAX_VERTICES; ++i){
+         for (int j = 0; j < right.MAX_VERTICES ; ++j){
+            T[i][j] = right.T[i][j];
+         }
+      }
+      for(int v = 1; v < right.size; v++){
+         vertices[v] = right.vertices[v];
+      }
+   }
+   return *this;
+}
+
+Graph:: ~Graph(){
+   for(int v = 1; v < size; v++){
+      EdgeNode *temp = vertices[v].edgeHead;
+      while(temp != nullptr){
+         delete temp;
+         temp = temp->nextEdge;
+      }
+     delete vertices[v].data;
+   }
+}
 
    //-------------------------------- buildGraph ---------------------------------
    // Builds a graph by reading data from an ifstream
@@ -25,9 +60,12 @@ void Graph::buildGraph(ifstream& infile) {
    
       // get descriptions of vertices
    for (int v = 1; v <= size; v++) {
-      infile.getline(..., ..., '\n');      // read descriptions (use of this method is not mandatory)
-      ...                                  // store appropriately
+      VertexNode node ;
+      vertices[v] = node;
+      infile >> node.data->v;      // read descriptions (use of this method is not mandatory)
+                                   // store appropriately
    }
+   
    
       // fill cost edge array
    int src = 1, dest = 1, weight = 1;
@@ -39,3 +77,65 @@ void Graph::buildGraph(ifstream& infile) {
    }
 }
 
+   //For insertEdge, replace any previous edge that existed between the two vertices.
+void Graph::insertEdge(int src, int dest, int weight){
+   if(vertices[src].edgeHead == nullptr){
+      vertices[src].edgeHead = new EdgeNode;
+      vertices[src].edgeHead->adjVertex = dest;
+      vertices[src].edgeHead->weight = weight;
+      vertices[src].edgeHead->nextEdge = nullptr;
+   }
+   else{
+      bool edgeExists = false;
+      EdgeNode *curr = vertices[src].edgeHead;
+      while(curr != nullptr){
+         if(curr->adjVertex == dest){
+            curr->weight = weight;
+            edgeExists = true;
+            break;
+         }
+         curr = curr->nextEdge;
+      }
+      if(!edgeExists){
+         EdgeNode *e1 = new EdgeNode;
+         e1->adjVertex = dest;
+         e1->weight = weight;
+         e1->nextEdge = nullptr;
+         curr = vertices[src].edgeHead;
+         while(curr->nextEdge != nullptr){
+            curr = curr->nextEdge;
+         }
+         curr->nextEdge = e1;
+      }
+   }
+   
+}
+void Graph::removeEdge(int src, int dest){
+   if(vertices[src].edgeHead != nullptr){
+      if(vertices[src].edgeHead->adjVertex == dest){
+         EdgeNode *temp = vertices[src].edgeHead;
+         vertices[src].edgeHead = vertices[src].edgeHead->nextEdge; //set edgeHead to the nextEdge
+         delete temp;
+      }
+      EdgeNode *curr = vertices[src].edgeHead;
+      while(curr->nextEdge != nullptr){
+         if(curr->nextEdge->adjVertex==dest){
+            EdgeNode *temp = curr->nextEdge;
+            curr->nextEdge = curr->nextEdge->nextEdge;
+            delete temp;
+           // break;
+         }
+         curr = curr->nextEdge;
+      }
+   }
+}
+void Graph::findShortestPath(){
+   
+}
+
+void Graph::display(int start, int end) const{
+   
+}
+void Graph::displayAll() const{
+   
+}
--- Assignment3/Assignment3/Graph.h
+++ Assignment3/Assignment3/Graph.h
@@ -1,11 +1,5 @@
-//
-//  Graph.hpp
-//  Assignment3
-//
-//  Created by Melinda Johnson on 2/4/20.
-//  Copyright © 2020 Melinda Johnson. All rights reserved.
-//
 
+//assume that the input is properly formatted
 #ifndef Graph_h
 #define Graph_h
 
@@ -23,13 +17,14 @@ private:
       EdgeNode *nextEdge;
    };
    
+   struct Vertex{
+      std::string v;
+   };
+   
    struct VertexNode {
       EdgeNode *edgeHead;      // head of the list of edges
       Vertex *data;      // store vertex data here
    };
-   
-      // array of VertexNodes
-   VertexNode vertices[MAX_VERTICES];
    
       // table of information for Dijkstra's algorithm
    struct Table {
@@ -37,12 +32,79 @@ private:
       int dist;            // shortest known distance from source
       int path;            // previous vertex in path of min dist
    };
-   
+     
+   VertexNode vertices[MAX_VERTICES];  // array of VertexNodes
    int size;               // number of vertices in the graph
    Table T[MAX_VERTICES][MAX_VERTICES];
       // stores visited, distance, path -
       // two dimensional in order to solve
       // for all sources
+   
+public:
+   /**
+    //-------------------------- Default constructor for class Graph ------------------------------------//
+    Preconditions: None
+    Postconditions:
+    */
+   Graph();
+   /**
+    //-------------------------- Copy constructor for class Graph ------------------------------------//
+    Preconditions:
+    Postconditions:
+    */
+   Graph(Graph& obj1);
+   /**
+    //-------------------------- Destructor  for class graph ------------------------------------//
+    Preconditions:A graph object is created
+    Postconditions: deallocate the elements stored in the Graph class
+    */
+   virtual ~Graph();
+   /**
+    //-------------------------- Overloaded assignment operator =  ------------------------------------//
+    Preconditions: A graph object is created
+    Postconditions: *this is assigned the same data as in the right object but at a different memory location
+    @return graph object which is a deep copy of the object right
+    */
+   const Graph operator= (const Graph& right);
+   /**
+    //-------------------------- buildGraph ------------------------------------//
+    Preconditions:
+    Postconditions:
+    */
+   void buildGraph(std::ifstream& file);
+   /**
+    //-------------------------- insertEdge ------------------------------------//
+    Preconditions:
+    Postconditions:
+    */
+   //For insertEdge, replace any previous edge that existed between the two vertices.
+   void insertEdge(int src, int dest, int weight);
+   /**
+    //-------------------------- removeEdge ------------------------------------//
+    Preconditions:
+    Postconditions:
+    */
+   void removeEdge(int, int);
+   /**
+    //-------------------------- findShortestPath ------------------------------------//
+    Preconditions:
+    Postconditions:
+    */
+   void findShortestPath(); //method that computes all shortest paths
+   //Use recursion, not a container to display a path. Remember that you work backwards from the destination to the source to recover the path.
+   /**
+    //-------------------------- display ------------------------------------//
+    Preconditions:
+    Postconditions:
+    */
+   void display(int start, int end) const;
+   /**
+    //-------------------------- displayAll ------------------------------------//
+    Preconditions:
+    Postconditions:
+    */
+   void displayAll() const; //output a table formatted with description from to dist and path
+   
 };
 
 #endif /* Graph_hpp */
